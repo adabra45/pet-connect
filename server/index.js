8a434d5727b4e34878e94334d1e182f592298ead
Anshul Dabra <adabra004@gmail.com>
2026-08-10 00:35:21 +0530

    Connect MongoDB Atlas successfully

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
 
-
+//Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err.message);
+  });
 
 app.get('/', (req, res) => {
   res.send('Pet Connect Backend is running!');
