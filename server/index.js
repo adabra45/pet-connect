a85a89af0480788f50dc9e6e7aa269cac39dc6ba
Anshul Dabra <adabra004@gmail.com>
2026-08-25 13:39:00 +0530

    Fix Socket.io CORS for production

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
   cors: {
     origin: ['http://localhost:5173', 'https://pet-connect-rosy.vercel.app'] // React frontend URL
     methods: ['GET', 'POST'],
+    credentials: true
   },
 });
 
