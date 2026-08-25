969e4b47785ae39c6b2412b9024e351c3198dcf3
Anshul Dabra <adabra004@gmail.com>
2026-08-25 13:42:05 +0530

    Fix Socket.io CORS for production

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://pet-connect-rosy.vercel.app'] // React frontend URL
+    origin: ['http://localhost:5173', 'https://pet-connect-rosy.vercel.app'], // React frontend URL
     methods: ['GET', 'POST'],
     credentials: true
   },
